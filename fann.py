9f282ff219a338ef564df5a64616a5022947c28e
uc666 <134004137+uc666@users.noreply.github.com>
2024-10-05 03:33:00 +0800

    Update fann.py

--- fann.py
+++ fann.py
@@ -43,9 +43,6 @@ try:
         # 修改内容
         data["wallpaper"] ="http://www.kf666888.cn/api/tvbox/img"
         data["warningText"] ="烟笼寒水月笼沙，夜泊秦淮近酒家。"
-        data["parses"] =[
-    {"name":"解析聚合","type":3,"url":"Demo"},{"name":"Web聚合","type":3,"url":"Web"},{"name":"Json轮询","type":2,"url":"Sequence"},{"name":"Json并发","type":2,"url":"Parallel"},{"name":"解析1","type":1,"url":"http://pan.qiaoji8.com/tvbox/neibu.php?url=","ext":{"flag":["qq","腾讯","qiyi","爱奇艺","奇艺","youku","优酷","tucheng","sohu","搜狐","letv","乐视","mgtv","芒果","tnmb","seven","yzm","aliyun","RJuMao","bilibili","1905","xinvip","XAL","qiqi","XALS","YuMi-vip"]}},{"name":"解析2","url":"https://jx.xmflv.com/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"解析3","url":"https://jx.xmflv.cc/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"90","url":"https://www.8090g.cn/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"7K","url":"https://jx.7kjx.com/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"盘古","url":"https://www.pangujiexi.com/jiexi/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"逍遥","url":"https://jx.m3u8.pw/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"80","url":"https://www.8090.la/8090/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"CK","url":"https://www.ckplayer.vip/jiexi/?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"play","url":"https://www.playm3u8.cn/jiexi.php?url=","type":0,"ext":{"flag":["qiyi","imgo","爱奇艺","奇艺","qq","qq 预告及花絮","腾讯","youku","优酷","pptv","PPTV","letv","乐视","leshi","mgtv","芒果","sohu","xigua","fun","风行"]},"header":{"User-Agent":"Mozilla/5.0"}},{"name":"嗅探1","type":0,"url":"https://jx.m3u8.tv/jiexi/?url=","ext":{"flag":["qq","腾讯","qiyi","iqiyi","爱奇艺","奇艺","youku","yk","优酷","mgtv","imgo","芒果"]}},{"name":"嗅探2","type":0,"url":"https://www.yemu.xyz/?url="}
-  ]
         data["logo"] = "./jar/logo.gif"
         data["lives"] = [
             {
@@ -92,23 +89,6 @@ try:
       "ext": "./json/农民影视.json"
     }
             new_site2 = {
-      "key": "freeok",
-      "name": "🦁freeOK┃独家",
-      "type": 3,
-      "changeable": 1,
-      "click": 'document.querySelector("#playleft iframe").contentWindow.document.querySelector("#start").click();',
-      "api": "csp_FreeOK",
-      "jar": "./jar/08015.jar;md5;d797a2aea119c4c5e217172e274d72f6"
-    }
-            new_site3 = {
-      "key": "csp_SP360",
-      "name": "🐠360┃1080P",
-      "type": 3,
-      "changeable": 0,
-      "api": "csp_SP360",
-      "jar": "./jar/08015.jar;md5;d797a2aea119c4c5e217172e274d72f6"
-    }
-            new_site4 = {
       "key": "追剧影视",
       "name": "🏅追剧┃不卡",
       "type": 3,
@@ -119,36 +99,8 @@ try:
       "jar": "./jar/0914.jar;md5;03c83adc48ceacbda2ab7fae8b1c6597",
       "ext": "http://z.kzjtv.com/"
     }
-            new_site5 = {
-      "key": "csp_瓜子",
-      "name": "🌰瓜子┃1080P",
-      "type": 3,
-      "api": "csp_Gz360",
-      "searchable":1,
-      "playerType":2,
-      "timeout":30,
-      "changeable":0,
-      "jar": "./jar/08015.jar;md5;d797a2aea119c4c5e217172e274d72f6"
-    }
-            new_site6 = {
-      "key": "hipy_js_腾云驾雾[官]",
-      "name": "🌞腾腾┃解析",
-      "type": 3,
-      "api": "http://我不是.摸鱼儿.top/json/js/drpy.min.js",
-      "searchable":1,
-      "quickSearch":1,
-      "filterable":1,
-      "order_num":0,
-      "changeable":0,
-      "jar": "./jar/08015.jar;md5;d797a2aea119c4c5e217172e274d72f6",
-      "ext": "http://www.mpanso.com/%E5%B0%8F%E7%B1%B3/tx.js"
-    }
             data["sites"].insert(0, new_site)
             data["sites"].insert(3, new_site2)
-            data["sites"].insert(4, new_site3)
-            data["sites"].insert(5, new_site4)
-            data["sites"].insert(6, new_site5)
-            data["sites"].insert(7, new_site6)
                         
         else:
             print('"sites" 键不在数据中')
