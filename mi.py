603430ca44e44770524068b58119b4919fe3946f
uc666 <134004137+uc666@users.noreply.github.com>
2024-10-21 01:25:02 +0800

    Update mi.py

--- mi.py
+++ mi.py
@@ -100,7 +100,12 @@ try:
             "quickSearch": 1,
             "changeable": 1,
             "jar": "./jar/yu.jar;md5;eb7676582d076a5505be5f23530e4f4d",
-            "ext":{"影片类型":"看电视吧","简介":"请勿相信视频中广告。+<div class=\"stui-pannel_bd\"&&</div>","分类url":"https://www.567dyy.com/list/{cateId}/area/{area}/by/{by}/class/{class}/page/{catePg}/year/{year}.html","分类":"电影$1#电视剧$2#综艺$3#动漫$4#短剧$5"}
+            "ext": {
+        "影片类型": "看电视吧",
+        "简介": "请勿相信视频中广告。+<div class=\"stui-pannel_bd\"&&</div>",
+        "分类url": "https://www.567dyy.com/list/{cateId}/area/{area}/by/{by}/class/{class}/page/{catePg}/year/{year}.html",
+        "分类": "电影$1#电视剧$2#综艺$3#动漫$4#短剧$5"
+      }
         }
         new_site3 = {
             "key": "qwqfun",
@@ -120,13 +125,13 @@ try:
             "searchable": 1,
             "changeable": 1,
             "categories": [
-    "大陆综艺",
-    "国产剧",
-    "香港剧",
-    "爽文短剧",
-    "喜剧片",
-    "国产动漫"
-   ]
+        "大陆综艺",
+        "国产剧",
+        "香港剧",
+        "爽文短剧",
+        "喜剧片",
+        "国产动漫"
+      ]
         }
         new_site5 = {
             "key": "天天弹幕",
